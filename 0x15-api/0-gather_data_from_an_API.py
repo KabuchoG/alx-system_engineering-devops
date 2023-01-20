0f51b9db390bf267e8de5ee0cffcfa6fbe3c42d4
George Kabucho <105174643+KabuchoG@users.noreply.github.com>
2023-01-20 06:37:41 -0800

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,31 @@
-#!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+#!/usr/bin python3
+"""REST API"""
+
+import json
 import requests
 import sys
 
+def return_todo_list(employee_id):
+    """Returns a list of to dos for an employee"""
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    res = requests.get(url)
+    to_do_list = json.loads(res.text)
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_res = requests.get(user_url)
+    user_details = json.loads(user_res.text)
+    tasks = len(to_do_list)
+    tasks_complete = [task for task in to_do_list if task["completed"] is True]
+    complete = len(tasks_complete)
+    print("Employee {} is done with tasks({}/{}):".format(user_details["name"], complete, tasks))
+    for n in tasks_complete:
+        print("\t{}".format(n["title"]))
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    if len(sys.argv) !=2:
+        print("Please provide a userID")
+    else:
+        try:
+            id = int(sys.argv[1])
+        except Exception:
+            print("Please privide a valid input")
+    return_todo_list(id)
+    
